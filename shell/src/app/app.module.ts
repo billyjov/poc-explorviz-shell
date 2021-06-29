b98dab6d67a79e5332ad400190186bebb7f0acc8
Billy Lando <b.lando@neusta.de>
2021-06-29 21:44:01 +0200

    feat: add ngBootstrap

--- shell/src/app/app.module.ts
+++ shell/src/app/app.module.ts
@@ -7,14 +7,8 @@ import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { LandscapesComponent } from './landscapes/landscapes.component';
 import { LoginComponent } from './login/login.component';
-// import { AuthConfig, AuthModule } from '@auth0/auth0-angular';
-import { environment } from 'src/environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-// const config: AuthConfig =  {
-//   domain: environment.auth0.domain,
-//   clientId: environment.auth0.clientId,
-//   redirectUri: environment.auth0.routeAfterLogin,
-// }
 
 @NgModule({
   declarations: [
@@ -27,9 +21,7 @@ import { environment } from 'src/environments/environment';
     BrowserModule,
     LazyElementsModule,
     AppRoutingModule,
-    // AuthModule.forRoot(
-    //   config
-    // )
+    NgbModule,
   ],
   providers: [],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
