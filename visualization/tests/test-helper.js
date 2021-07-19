f099d7a50e13de5b33a70fbdbca7ebf81bc148d8
Billy Lando <b.lando@neusta.de>
2021-07-19 14:35:29 +0200

    feat(visualization): init js app

---
+++ visualization/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'visualization/app';
+import config from 'visualization/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
