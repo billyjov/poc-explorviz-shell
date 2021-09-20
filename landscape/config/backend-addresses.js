a508989227b8b9644063604bb67de00af69fd17a
Billy Lando <billy_lando@yahoo.com>
2021-09-21 01:04:20 +0200

    chore: clean up backend addresses files

--- landscape/config/backend-addresses.js
+++ landscape/config/backend-addresses.js
@@ -1,13 +1,6 @@
 module.exports = {
-  // landscapeService: 'http://localhost:32680',
-  // landscapeService: 'http://localhost:8082',
-  landscapeService: 'http://192.168.1.4:8082',
-  // traceService: 'http://localhost:32681',
-  // traceService: 'http://localhost:8083',
-  traceService: 'http://192.168.1.4:8083',
-  // userService: 'http://localhost:32682',
-  // userService: 'http://localhost:8084',
-  userService: 'http://192.168.1.4:8084',
-  // collaborativeService: 'ws://localhost:8085',
-  collaborativeService: 'ws://192.168.1.4:8085',
+  landscapeService: 'http://192.168.33.3:8082',
+  traceService: 'http://192.168.33.3:8083',
+  userService: 'http://192.168.33.3:8081',
+  collaborativeService: 'ws://192.168.33.3:8085',
 };
--- visualization/config/backend-addresses.js
+++ visualization/config/backend-addresses.js
@@ -1,10 +1,6 @@
 module.exports = {
-  // landscapeService: 'http://localhost:32680',
   landscapeService: 'http://192.168.33.3:8082',
-  // traceService: 'http://localhost:32681',
   traceService: 'http://192.168.33.3:8083',
-  // userService: 'http://localhost:32682',
   userService: 'http://192.168.33.3:8081',
-  // collaborativeService: 'ws://localhost:8085',
   collaborativeService: 'ws://192.168.33.3:8085',
 };
