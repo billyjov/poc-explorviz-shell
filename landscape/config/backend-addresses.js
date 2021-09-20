417d64a481cdd5aa678a70bcf058274788b60e51
Billy Lando <billy_lando@yahoo.com>
2021-09-21 01:16:19 +0200

    chore: clean up be addresses

--- landscape/config/backend-addresses.js
+++ landscape/config/backend-addresses.js
@@ -3,5 +3,4 @@ module.exports = {
   traceService: 'http://192.168.33.3:8083',
   userService: 'http://192.168.33.3:8081',
   collaborativeService: 'ws://192.168.33.3:8085',
-  collaborativeService: 'ws://192.168.33.3:8085',
 };
