c3e86b76be127d0a912dc01fb8d36b14c3957e0a
Billy Lando <billy_lando@yahoo.com>
2021-09-21 23:46:18 +0200

    feat(shell): enable app alias on top of viz

--- shell/src/app/shell-outlet/shell-outlet/shell-outlet.component.ts
+++ shell/src/app/shell-outlet/shell-outlet/shell-outlet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { fromEvent, Subscription } from 'rxjs';
@@ -10,7 +10,7 @@ import { AuthService } from 'src/app/core/auth/auth.service';
   templateUrl: './shell-outlet.component.html',
   styleUrls: ['./shell-outlet.component.scss'],
 })
-export class ShellOutletComponent implements OnInit {
+export class ShellOutletComponent implements OnInit, OnDestroy {
   public isNavbarCollapsed = true;
   public user = this.authService.user;
 
@@ -20,19 +20,21 @@ export class ShellOutletComponent implements OnInit {
 
   ngOnInit() {
     fromEvent(window, 'ember-app:landscapes').subscribe((event) => {
-      console.log('event was sent from sub app:::: ', event);
       this.router.navigateByUrl('landscapes');
     });
 
     this.setLandscapeVisualization();
   }
 
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
   private setLandscapeVisualization(): void {
     this.subscriptions.add(
       fromEvent(window, 'landscapes:navigate-to-visualization').subscribe(
-        (event: Partial<CustomEvent>) => {
+        (_event: Partial<CustomEvent>) => {
           this.navigateToRoute('/visualization');
-          console.log('event from landscape: ', event);
         }
       )
     );
@@ -48,5 +50,13 @@ export class ShellOutletComponent implements OnInit {
       window.location.href =
         window.location.protocol + '//' + window.location.host + path;
     });
+  }
+
+  public isVisualizationRoute(): boolean {
+    return this.router.url === '/visualization';
+  }
+
+  public get appAlias(): string {
+    return JSON.parse(localStorage.getItem('currentLandscapeToken') as string)?.alias;
   }
 }
