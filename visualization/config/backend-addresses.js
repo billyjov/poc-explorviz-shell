6d0df388ac65b4dfc9dcd1915402ee8371e391c5
Billy Lando <billy_lando@yahoo.com>
2021-09-23 21:33:04 +0200

    fix(visualization/landscapes): adapt userService port and upgrade auto-import

--- visualization/config/backend-addresses.js
+++ visualization/config/backend-addresses.js
@@ -1,6 +1,6 @@
 module.exports = {
   landscapeService: 'http://192.168.33.3:8082',
   traceService: 'http://192.168.33.3:8083',
-  userService: 'http://192.168.33.3:8081',
+  userService: 'http://192.168.33.3:8084',
   collaborativeService: 'ws://192.168.33.3:8085',
 };
