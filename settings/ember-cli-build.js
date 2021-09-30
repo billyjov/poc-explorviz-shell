056b9b1a7e03015b9bce7741f2945adbe1960818
Billy Lando <billy_lando@yahoo.com>
2021-09-30 23:45:48 +0200

    fix(settings): add missing helpers and adjust deps

--- settings/ember-cli-build.js
+++ settings/ember-cli-build.js
@@ -57,7 +57,9 @@ module.exports = function (defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  // app.import('node_modules/bootstrap/dist/js/bootstrap.min.js');
+
+  app.import('node_modules/jquery/dist/jquery.min.js');
+  app.import('node_modules/bootstrap/dist/js/bootstrap.min.js');
   app.import(
     'node_modules/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css'
   );
