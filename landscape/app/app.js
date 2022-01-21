66f98df4e78247693c10c4deb4f73e1ba8ac0b19
Billy Lando <billy_lando@yahoo.com>
2022-01-21 17:19:13 +0100

    fix(landscape): change backend addresses to default online

--- landscape/app/app.js
+++ landscape/app/app.js
@@ -9,7 +9,6 @@ export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
   Resolver = Resolver;
-  autoboot = false;
 }
 
 loadInitializers(App, config.modulePrefix);
--- landscape/config/backend-addresses.js
+++ landscape/config/backend-addresses.js
@@ -1,6 +1,6 @@
 module.exports = {
-  landscapeService: 'http://192.168.33.3:8082',
+  landscapeService: 'http://samoa.se.informatik.uni-kiel.de:8090',
   traceService: 'http://192.168.33.3:8083',
-  userService: 'http://192.168.33.3:8084',
+  userService: 'http://samoa.se.informatik.uni-kiel.de:8090',
   collaborativeService: 'ws://192.168.33.3:8085',
 };
